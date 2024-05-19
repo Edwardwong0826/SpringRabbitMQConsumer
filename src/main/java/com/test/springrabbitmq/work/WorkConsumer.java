84cf0b2b6c3683d4db63d0d0d3639043afb88e42
Edwardwong0826 <edwardwong_94@outlook.com>
2024-05-19 20:43:31 +0800

    RabbitMQ Consumer Prefetch and Dispatch
    Add Info about how to use Consumer Prefetch to set fair dispatch or unfair dispatch

--- src/main/java/com/test/springrabbitmq/work/WorkConsumer.java
+++ src/main/java/com/test/springrabbitmq/work/WorkConsumer.java
@@ -25,8 +25,24 @@ public class WorkConsumer
         System.out.println("Simple queue message = " + message);
     }
 
-    // We can set in spring rabbit config prefetch = x
-    // To force the consumer only fetch from the queue when consume finish, thus those higher performance consumer can handle more queue messages and speed up process
+    // https://www.rabbitmq.com/tutorials/tutorial-two-java
+    // in RabbitMQ, all queue are by default was fair dispatch
+    // basicQos = 0 is fair dispatch in RabbitMQ library code
+    // set the prefetchCount = 1 or prefetchCount > 0 to become non-fair dispatch
+    // int prefetchCount = 1;
+    // channel.basicQos(prefetchCount);
+    // 不设置 basicQos的话是一次性平均分发给所有的队列，设置之后限制了一次分发消息的数量，在设置手动确认机制，这样在你没有提交已经处理好的消息的时候是不会给你分发消息的。实现的不公平分发。
+    // so need to enable 手动确认机制 for non-fair dispatch
+
+    // https://www.rabbitmq.com/docs/consumer-prefetch
+    // 准确来说 prefetchCount 是通道中允许的未确认消息的最大数量
+    // prefetch basically is the max amount of unacknowledged data allow to exist on the RabbitMQ channel
+    // so that RabbitMQ will not send the data to that queue(as channel is unacknowledged data is full) anymore
+    // if set auto ack mode and set prefetch value, data in the channel will lose if the queue got error or down
+    // but if we use manual ack then it will still requeue back again
+    
+    // We can set in spring rabbit config prefetch = 1 or x
+    // To force the consumer only fetch from the queue when consume finish (which is finish consume and send back acknowledge), thus those higher performance consumer can handle more queue messages and speed up process
     // So need to wait slow consumer with round-rabin scheduling, so it doesn't accumulate on queue, this is one of the way, add more consumer enable this prefetch config
     @RabbitListener(queues = "work.queue")
     public void listenWorkQueue1(String message) throws InterruptedException {
